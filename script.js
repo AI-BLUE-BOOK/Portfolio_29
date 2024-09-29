534fdd89d1a785d2abff867b5ebe6b7848e66bc7
Asus_Abhishek <dubeyabhi065@gmail.com>
2024-09-29 22:42:10 +0530

    updted

---
+++ script.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
